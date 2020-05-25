07f0b7b6b295a1d22df4e7ac4be96a876d8af580
Ayden Cauchi <ayden@modimages.com>
2020-05-25 15:29:37 -0400

    trying to connect mongo atlas again

--- app.js
+++ app.js
@@ -25,13 +25,16 @@ const campground_routes = require('./routes/campgrounds'),
       auth_routes       = require('./routes/index');
 //=============================
 
+//=============================
 // CONNECTING THE DATABASE
+//local route: mongodb://localhost:27017/yelp_camp
 mongoose.connect("mongodb+srv://Shmayden7:1760Ayden.@cluster0-edxa9.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, 
 useUnifiedTopology: true, useCreateIndex: true}).then(() => {
     console.log('D.B. Connected!');
 }).catch(err => {
     console.log('ERROR:', err.message);
 });
+//=============================
 
 app.use(body_parser.urlencoded({extended: true}));
 // this will make you not have to type .ejs
